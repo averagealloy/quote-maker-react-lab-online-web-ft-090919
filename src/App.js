0b6a73dd5d7d7cf7ed135fb4df8834bd233cbc58
averagealloy <m.mctighe10@gmail.com>
2020-02-06 13:21:00 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class App extends Component {
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="container-fluid">
+//         <div className="row title justify-content-center" style={{ paddingTop: '12px' }}>
+//           <h1>Quote Maker</h1>
+//         </div>
+//         <hr />
+//         {/*
+//           TODO:
+
+//           Render QuoteForm and Quotes Components
+//          */}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+
+import React, { Component } from 'react';
+import QuoteForm from './components/QuoteForm';
+import Quotes from './containers/Quotes';
+
+export class App extends Component {
   render() {
     return (
       <div className="container-fluid">
@@ -8,11 +34,8 @@ class App extends Component {
           <h1>Quote Maker</h1>
         </div>
         <hr />
-        {/*
-          TODO:
-
-          Render QuoteForm and Quotes Components
-         */}
+        <QuoteForm />
+        <Quotes />
       </div>
     );
   }
